a03d05c0653e7604e8f8f72fd1e3b102fc5db24c
Syed Shahadat <sm.work12@gmail.com>
2022-02-23 16:58:41 +0600

    add useCost

--- packages/api-client/src/index.server.ts
+++ packages/api-client/src/index.server.ts
@@ -2,7 +2,7 @@ import { apiClientFactory } from '@vue-storefront/core';
 import type { Setttings, Endpoints } from './types';
 import axios from 'axios';
 
-import * as costResult from './api/wgLcaCost';
+import { costResult } from './api/wgLcaCost';
 
 const onCreate = (settings) => {
   const client = axios.create({
